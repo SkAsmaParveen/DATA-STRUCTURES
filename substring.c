3b6bf17a9cfeffe80ea8813c3bbefa9edac53cc0
SHAIK ASMA PARVEEN <112798058+SkAsmaParveen@users.noreply.github.com>
2023-01-04 19:59:37 +0530

    Add files via upload

---
+++ substring.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int count = 0, max = 0;
+    char s[20];
+    printf("Enter a string: ");
+    scanf(" %[^\n]", s);
+    int n = strlen(s);
+    for (int i = 0; i < n; ++i)
+    {
+        if (s[i] == '1')
+        {
+            count++;
+        }
+        else
+        {
+            if (count > max)
+                max = count;
+            count = 0; 
+        }
+    }
+    if (count > max)
+    {
+        max = count;
+    }
+    printf("Largest substring is of length: %d\n", max);
+}
